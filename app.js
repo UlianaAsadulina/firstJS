606ac3893718f5bb280520d604b1e4f2d049f5bd
UlianaAsadulina <u.asadulina@icloud.com>
2024-11-22 12:41:39 -0500

    Add console log statements

--- app.js
+++ app.js
@@ -45,35 +45,33 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
 
 
-//   Check if all numbers are divisible by 5. 
-// Cache the result in a variable.
+//  Check if all numbers are divisible by 5. 
+
 let resultDivisionN1By5 = n1 % 5 === 0;
 let resultDivisionN2By5 = n2 % 5 === 0;
 let resultDivisionN3By5 = n3 % 5 === 0;
 let resultDivisionN4By5 = n4 % 5 === 0;
 
-console.log("Const n1= " + n1 + " divisible by 5: " + resultDivisionN1By5); 
-console.log("Const n2= " + n2 + " divisible by 5: " + resultDivisionN2By5);
-console.log("Const n3= " + n3 + " divisible by 5: " + resultDivisionN3By5);
-console.log("Const n3= " + n4 + " divisible by 5: " + resultDivisionN4By5);
+// Cache the result in a variable.
 let allDivisibleBy5 = resultDivisionN1By5 && resultDivisionN2By5 && resultDivisionN3By5 && resultDivisionN4By5;
-console.log("All constants divisible by 5: " + allDivisibleBy5); 
+
+console.log(`All constants divisible by 5: ${allDivisibleBy5}`); 
 
 // Check if the first number is larger than the last. 
 // Cache the result in a variable.
 let n1GreaterN4 = n1 > n4;
-console.log("First number is greater then last number: " + n1GreaterN4);
+console.log(`First number is greater then last number: ${n1GreaterN4}`);
 
 // Accomplish this arithmetic chain:
 // Subtract the first number from the second number.
 let subN2N1=n2-n1;
-console.log(subN2N1);
+
 // Multiply the result by the third number.
 let multRezByN3 = subN2N1*n3;
-console.log(multRezByN3);
+
 // Find the remainder of dividing the result by the fourth number.
 let rezMultByN4 = multRezByN3 % n4;
-console.log(rezMultByN4);
+console.log(`Remaider of dividing (n2-n1)*n3 by ${n4} is ${rezMultByN4}`);
 
 // Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. 
 // Rename the variable as appropriate.
@@ -87,5 +85,5 @@ const isUnder25 = n1 < 25 && n2 < 25 && n3 < 25 && n4 < 25;
 // We teste for "isUnder25" as an alternative.
 isValid = isSum50 && isTwoOdd && isUnder25 && isUnique;
 
-// Finally, log the results.
-console.log(isValid);
+// Finally results.
+console.log (`The statement what all four numbers are valid according to the provided criteria is  ${isValid}.`);
